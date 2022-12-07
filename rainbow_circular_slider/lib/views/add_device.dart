899059c34ab3d611f10a2d34221ee117b3d0bb53
chuikelvin <chuikelvin2@gmail.com>
2022-12-07 17:29:47 +0300

    espressif logo

--- rainbow_circular_slider/lib/views/add_device.dart
+++ rainbow_circular_slider/lib/views/add_device.dart
@@ -14,32 +14,49 @@ class _AddDeviceState extends State<AddDevice> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(excludeHeaderSemantics: true),
-      body: Container(
-        // color:Colors.red,
-        child: ListView.builder(
-          itemCount:3,
-          itemBuilder: ((context, index) {
-            return Center(
-              child: Container(
-                width:MediaQuery.of(context).size.width- 40,
-                height:80,
-                margin:EdgeInsets.symmetric(vertical:10),
-                decoration:BoxDecoration(
-                  border: Border.all(color: Color.fromARGB(255, 138, 138, 138), width: 1.0),
-                  borderRadius:BorderRadius.circular(12),
-                  color:Colors.amber,
-                ),
-                child:Center(
-                  child: Text(
-                    "hello world",
-                    // textAlign:Alignment.center
-                  ),
-                )
-              ),
-            );
-          }))
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Center(child: Text('add a device')),
       ),
+      body: Container(
+          // color:Colors.red,
+          child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: ((context, index) {
+                return Center(
+                  child: Container(
+                      width: MediaQuery.of(context).size.width - 40,
+                      // height: 80,
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Color.fromARGB(255, 138, 138, 138),
+                            width: 1.0),
+                        borderRadius: BorderRadius.circular(12),
+                        color: index % 2 == 0
+                            ? Color.fromARGB(66, 212, 212, 212)
+                            : Color.fromARGB(66, 208, 157, 85),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(
+                              'assets/images/espressif.png',
+                              // height: (MediaQuery.of(context).size.height),
+                              // width: (MediaQuery.of(context).size.width),
+                              scale: 0.8,
+                            ),
+                            Text(
+                              "hello world",
+                              // textAlign:Alignment.center
+                            )
+                          ],
+                        ),
+                      )),
+                );
+              }))),
     );
   }
 }
